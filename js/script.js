f822f32e3288ea05d308808ad7dcdc8209e0858f
zaki358 <t.yamazaki.1129@gmail.com>
2022-03-01 00:57:38 +0900

    改造

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
-$(function () {
-
-});
+//jQuery( function( $ ) {
+	//$( ".js-hamburger" ).on( "click", function() {
+		//$( this ).toggleClass( "is-open" );
+		//$( ".p-gmenu" ).toggleClass( "is-open" );
+		//$( "body" ).toggleClass( "is-open" );
+	//} );
+//} );
