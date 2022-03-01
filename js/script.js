184b305709f102847bc1152b6ad6dd673e421379
zaki358 <t.yamazaki.1129@gmail.com>
2022-03-02 01:11:45 +0900

    ヘッダー部分完成？

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
-//jQuery( function( $ ) {
-	//$( ".js-hamburger" ).on( "click", function() {
+jQuery( function( $ ) {
+	$( ".js-menu--open" ).on( "click", function() {
 		//$( this ).toggleClass( "is-open" );
-		//$( ".p-gmenu" ).toggleClass( "is-open" );
-		//$( "body" ).toggleClass( "is-open" );
-	//} );
-//} );
+		$( ".p-menu" ).addClass( "is-open" );
+		$( "body" ).addClass( "is-open" );
+		$( ".c-layer--sidebar" ).addClass( "is-open" );
+
+	} );
+	$( ".js-menu--close" ).on( "click", function() {
+		$( ".p-menu" ).removeClass( "is-open" );
+		$( "body" ).removeClass( "is-open" );
+		$( ".c-layer--sidebar" ).removeClass( "is-open" );
+	} );
+} );
